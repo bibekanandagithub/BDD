a328c2790eef4374db7fb3fec6c042ae5f8569b7
bibekananda.panigrahi@outlook.com <bibekananda.panigrahi@outlook.com>
2019-11-16 21:42:22 +0530

    BDD

---
+++ BDD/dynamicTables/DynamicTable.feature
@@ -0,0 +1,15 @@
+﻿Feature: DynamicTable
+	In order to avoid silly mistakes
+	As a math idiot
+	I want to be told the sum of two numbers
+
+Scenario: Create a new employee with mandatory details providing with Dyanamic keywords
+	#Given I have opened my Application
+	#Then I should see employee detils page
+	When i have new data i used  dynamic structure in order print
+	|Name	| Age | Phone     | Email                        |
+	|Bibek	|34   |8895591255 |bibekanand.panigrahi@bkfs.com |
+	|RAJU	|51   |123456789 |rahuk.panigrahi@bkfs.com |
+	|john	|30   |321654987 |dem.panigrahi@bkfs.com |
+	#And I click the save button
+	#Then I should see all the details saved in my application and DB
---
+++ BDD/dynamicTables/def_DynamicStructure.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+using TechTalk.SpecFlow.Assist;
+
+namespace BDD.dynamicTables
+{
+    [Binding]
+    public sealed class def_DynamicStructure
+    {
+        // For additional details on SpecFlow step definitions see https://go.specflow.org/doc-stepdef
+
+        [When(@"i have new data i used  dynamic structure in order print")]
+        public void WhenIHaveNewDataIUsedDynamicStructureInOrderPrint(Table table)
+        {
+            var Details = table.CreateDynamicSet();
+            foreach(var emp in Details)
+            {
+                Console.WriteLine("Name is {0}",emp.Name);
+                Console.WriteLine("Name is {0}", emp.Age);
+                Console.WriteLine("Name is {0}", emp.Phone);
+                Console.WriteLine("Name is {0}", emp.Email);
+
+            }
+
+        }
+
+    }
+}
